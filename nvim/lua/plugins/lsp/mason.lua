ecae02141ae4031844f55d1232f9a99b653e1d3a
STAR-H <ill201716@163.com>
2023-08-13 01:46:43 +0800

    add statusline current function

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,5 +1,52 @@
 return {
     {
+        "SmiteshP/nvim-navic",
+        dependencies= {"neovim/nvim-lspconfig"},
+        config = function ()
+            require("nvim-navic").setup {
+                icons = {
+                    File          = "󰈙 ",
+                    Module        = " ",
+                    Namespace     = "󰌗 ",
+                    Package       = " ",
+                    Class         = "󰠱 ",
+                    Method        = "ƒ ",
+                    Property      = "󰜢 ",
+                    Field         = " ",
+                    Constructor   = " ",
+                    Enum          = " ",
+                    Interface     = " ",
+                    Function      = "󰊕 ",
+                    Variable      = "󰀫 ",
+                    Constant      = "󰏿 ",
+                    String        = "󰀬 ",
+                    Number        = "󰎠 ",
+                    Boolean       = "◩ ",
+                    Array         = "󰅪 ",
+                    Object        = "󰅩 ",
+                    Key           = "󰌋 ",
+                    Null          = "󰟢 ",
+                    EnumMember    = " ",
+                    Struct        = "󰆧 ",
+                    Event         = " ",
+                    Operator      = "󰆕 ",
+                    TypeParameter = " ",
+                },
+                lsp = {
+                    auto_attach = false,
+                    preference = nil,
+                },
+                highlight = false,
+                separator = " > ",
+                depth_limit = 10,
+                depth_limit_indicator = "..",
+                safe_output = true,
+                lazy_update_context = false,
+                click = true
+            }
+        end
+    },
+    {
         "williamboman/mason.nvim",
         dependencies = "williamboman/mason-lspconfig.nvim",
         config = function()
