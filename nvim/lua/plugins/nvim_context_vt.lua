b33e575f18f4e3a521fd0620c8ee5b51b1a6e3b1
STAR-H <ill201716@163.com>
2024-08-12 22:21:40 +0800

    some rontinue fix
    1. add meld as difftool
    2. change tmux color from screen  to xterm
    3. add clang-format

--- nvim/lua/plugins/nvim_context_vt.lua
+++ nvim/lua/plugins/nvim_context_vt.lua
@@ -1,23 +1,17 @@
 return {
     "andersevenrud/nvim_context_vt",
     event = "VeryLazy",
+    ft = { 'cpp', 'lua', 'python' },
     config = function()
+        vim.api.nvim_set_hl(0, 'CustomContextVt', { fg = '#928374', bold = true, italic = true })
         require('nvim_context_vt').setup({
             enabled = true,
-
-            prefix = ' 𝓮𝓷𝓭 𝓸𝓯',
-
-            highlight = 'markdownLinkText',
-
+            prefix = '󰞘 𝓮𝓷𝓭 𝓸𝓯',
+            highlight = 'CustomContextVt',
             disable_ft = { 'markdown' },
-
             disable_virtual_lines = false,
-
             disable_virtual_lines_ft = { 'yaml' },
-
             min_rows = 10,
         })
-
-
     end,
 }
--- nvim/lua/plugins/vim-maximizer.lua
+++ nvim/lua/plugins/vim-maximizer.lua
@@ -1,4 +1,5 @@
 return {
     "szw/vim-maximizer",
+    enabled = false;
     keys = {{ "<space><space>", "<cmd>MaximizerToggle<cr>", desc = "Maximize/Minimize a split" }}
 }
