39582ccc48dd2e09d04432f744f889454584b5d2
dotfiles user <user@example.invalid>
2024-12-07 00:01:54 +0800

    NVIM: update bufferline lazy event and easyalign trigger key

--- nvim/lua/plugins/easy-align.lua
+++ nvim/lua/plugins/easy-align.lua
@@ -1,9 +1,6 @@
 return {
     "junegunn/vim-easy-align",
-    cmd = "EasyAlign",
-    config = function()
-        local opts = {desc = "Easy[A]lign", noremap = true, silent = true, nowait = true}
-        vim.keymap.set('n', 'ga', ":EasyAlign<CR>", opts)
-        vim.keymap.set('x', 'ga', ":EasyAlign<CR>", opts)
-    end
+    keys = {
+        {"ga", mode = {"n", "x"}, "<cmd>EasyAlign<cr>"},
+    }
 }
