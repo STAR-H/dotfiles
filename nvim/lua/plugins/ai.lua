25fa0983e5cd8d7b185591e8949d1e860c6b5804
dotfiles user <user@example.invalid>
2025-04-19 15:13:42 +0800

    nvim:add focus.nvim plugin replace before win highliht method

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,6 +1,7 @@
 return {
   {
     "yetone/avante.nvim",
+    enabled = not require("configs.utils").is_diff_mode(),
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     opts = {
--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -249,5 +249,93 @@ return {
       { "<leader><leader>", function() Snacks.image.hover() end, desc = "Snacks show image at cursor" },
       { "<leader>uz",       function() Snacks.zen() end,         desc = "Toggle Zen Mode" },
     }
-  }
+  },
+
+  {
+    'nvim-focus/focus.nvim',
+    enabled = not require("configs.utils").is_diff_mode(),
+    event = "VeryLazy",
+    version = '*',
+    config = function()
+      require("focus").setup({
+        enable = true,        -- Enable module
+        commands = false,      -- Create Focus commands
+        autoresize = {
+          enable = false,      -- Enable or disable auto-resizing of splits
+          width = 0,          -- Force width for the focused window
+          height = 0,         -- Force height for the focused window
+          minwidth = 0,       -- Force minimum width for the unfocused window
+          minheight = 0,      -- Force minimum height for the unfocused window
+          height_quickfix = 10, -- Set the height of quickfix panel
+        },
+        split = {
+          bufnew = false, -- Create blank buffer for new split windows
+          tmux = false, -- Create tmux splits instead of neovim splits
+        },
+        ui = {
+          number = false,                  -- Display line numbers in the focussed window only
+          relativenumber = false,          -- Display relative line numbers in the focussed window only
+          hybridnumber = false,            -- Display hybrid line numbers in the focussed window only
+          absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+
+          cursorline = true,               -- Display a cursorline in the focussed window only
+          cursorcolumn = false,            -- Display cursorcolumn in the focussed window only
+          colorcolumn = {
+            enable = false,                -- Display colorcolumn in the foccused window only
+            list = '+1',                   -- Set the comma-saperated list for the colorcolumn
+          },
+          signcolumn = true,               -- Display signcolumn in the focussed window only
+          winhighlight = true,            -- Auto highlighting for focussed/unfocussed windows
+        }
+      })
+
+      vim.api.nvim_set_hl(0, 'FocusedWindow', { link = 'Normal' })
+      vim.api.nvim_set_hl(0, 'UnfocusedWindow', { bg = '#3a3a3a' })
+
+      local ignore_filetypes = { 
+        "NvimTree",
+        "tagbar",
+        "undotree",
+        "vista_kind",
+        "vista_markdown",
+        "trouble",
+        "AvanteInput",
+        "AvanteSelectedFiles",
+        "Avante"
+      }
+      local ignore_buftypes = {
+        "nofile",
+        "prompt",
+        "popup",
+      }
+
+      local augroup =
+      vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+      vim.api.nvim_create_autocmd('WinEnter', {
+        group = augroup,
+        callback = function(_)
+          if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+            then
+              vim.w.focus_disable = true
+            else
+              vim.w.focus_disable = false
+            end
+          end,
+          desc = 'Disable focus autoresize for BufType',
+        })
+
+        vim.api.nvim_create_autocmd('FileType', {
+          group = augroup,
+          callback = function(_)
+            if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+              vim.b.focus_disable = true
+            else
+              vim.b.focus_disable = false
+            end
+          end,
+          desc = 'Disable focus autoresize for FileType',
+        })
+    end
+  },
 }
