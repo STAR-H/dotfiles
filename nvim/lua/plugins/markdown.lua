465358d804de063403f00290cd3b72ce2b6f7cf1
dotfiles user <user@example.invalid>
2026-06-04 20:23:49 +0800

    nvim: refine markdown preview layout

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -199,6 +199,9 @@ return {
       vim.g.mkdp_auto_close = 0
       vim.g.mkdp_auto_start = 0
       vim.g.mkdp_refresh_slow = 0
+      vim.g.mkdp_preview_options = {
+        disable_filename = 1,
+      }
     end,
   },
 }
